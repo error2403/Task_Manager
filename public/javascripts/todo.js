49a7a2d6af15f47a4cfd89bdd0ac5d0b47a39056
Alex R. <rusk.alex4@gmail.com>
2025-10-16 10:20:39 -0700

    fix add button
    add button had incorrect element IDs

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -30,15 +30,15 @@ list.addEventListener('click', function(ev) {
 // Create a new list item when clicking on the "Add" button
 function newTodoElement() {
     var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
+    var inputValue = document.getElementById("todoInput").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
     if (inputValue === '') {
         alert("You must write something!");
     } else {
-        document.getElementById("myUL").appendChild(li);
+        document.getElementById("todoList").appendChild(li);
     }
-    document.getElementById("myInput").value = "";
+    document.getElementById("todoInput").value = "";
 
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
