9924722eb689c4446d8ff38e21bb98e7f05e0ec2
Alex R. <rusk.alex4@gmail.com>
2025-11-14 15:53:37 -0700

    moved TODO list to right side of screen
    made a main div and added aside & content block to it & gave it flex property so they sit side by side on page

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -1,18 +1,17 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
+var todoList = document.getElementById("todoList");
+var todoItems = todoList.getElementsByTagName("li");
+for (var i = 0; i < todoItems.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+    todoItems[i].appendChild(span);    
 }
 
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
+for (var i = 0; i < close.length; i++) {
     close[i].onclick = function() {
         var div = this.parentElement;
         div.style.display = "none";
