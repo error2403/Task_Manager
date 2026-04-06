d852e6b8c2f93d1cadfde90cfea532512909643b
Alex R. <rusk.alex4@gmail.com>
2026-04-06 16:18:49 -0700

    functional list
    click to strikethrough works
    add & remove from list
    delete from list

--- todo.js
+++ todo.js
@@ -21,8 +21,7 @@ for (i = 0; i < close.length; i++) {
 }
 
 // Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
+taskListElement.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     }
